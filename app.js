4e3cdf62c9aca42911e22f2091abe0238bf6199e
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-10-06 17:05:30 +0700

    changed to switch/case instead of if

--- app.js
+++ app.js
@@ -5,32 +5,41 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const { getGas, broadcast, reply } = require("./helpful_functions.js");
+const gasNoti = setInterval(broadcast, 10000);
 
 app.use(bodyParser.urlencoded({ extende: false }));
 app.use(bodyParser.json());
 
 app.post("/webhook", async (req, res) => {
-  if (
-    req.body.events[0].message.text.localeCompare("gas", undefined, {
-      sensitivity: "accent",
-    }) === 0
-  ) {
-    let reply_token = req.body.events[0].replyToken;
-    console.log("incoming msg", req.body.events[0].message.text);
-    console.log("Getting gas fee...");
-    let gasFee = await getGas();
-    console.log("Gas Fee is ", gasFee);
-    console.log("Replying user...");
-    reply(reply_token, gasFee);
-  } else {
-    let reply_token = req.body.events[0].replyToken;
-    console.log("incoming msg", req.body.events[0].message.text);
-    reply(reply_token, req.body.events[0].message.text);
+  switch (req.body.events[0].message.text.toLowerCase()) {
+    case "gas":
+      let reply_token = req.body.events[0].replyToken;
+      let gasFee = await getGas();
+      reply(reply_token, gasFee);
+      break;
+    default:
+      let reply_token = req.body.events[0].replyToken;
+      console.log("incoming msg", req.body.events[0].message.text);
+      reply(reply_token, req.body.events[0].message.text);
+      break;
   }
+
+  // if (
+  //   req.body.events[0].message.text.localeCompare("gas", undefined, {
+  //     sensitivity: "accent",
+  //   }) === 0
+  // ) {
+  //   let reply_token = req.body.events[0].replyToken;
+  //   let gasFee = await getGas();
+  //   reply(reply_token, gasFee);
+  // } else {
+  //   let reply_token = req.body.events[0].replyToken;
+  //   console.log("incoming msg", req.body.events[0].message.text);
+  //   reply(reply_token, req.body.events[0].message.text);
+  // }
 
   res.sendStatus(200);
 });
 app.listen(port, () => {
   console.log("listening on port...", port);
-  setInterval(broadcast, 100000);
 });
