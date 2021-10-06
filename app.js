b34f138cbf6fdc3c18df511ed861dab7f9c9f22c
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-10-06 23:57:15 +0700

    added ping function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -17,6 +18,11 @@ const checkGasFeeBelow50gwei = setInterval(async () => {
     broadcast("cheapGas", gasFee);
   }
 }, 120000);
+
+const pingAppEvery29mins = setInterval(() => {
+  axios.get("https://eth-gas-line-chatbot.herokuapp.com/webhook");
+  console.log("...ping!");
+}, 17400000);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
