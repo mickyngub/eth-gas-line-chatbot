505bea5d4bb9d7b3dd9e98fd5a815a47f4fcc1b6
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-10-06 12:49:02 +0700

    fixed status typo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
-app.post("/webhook", (req, res) => res.sendStatus(2000));
+app.post("/webhook", (req, res) => res.sendStatus(200));
 app.listen(port);
