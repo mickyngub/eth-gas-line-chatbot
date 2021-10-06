086a906589c4ea4f12a148ef5ce8bb206381b6fa
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-10-06 14:14:16 +0700

    test getGas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const request = require("request");
 const app = express();
 const port = process.env.PORT || 4000;
@@ -35,24 +36,33 @@ const reply = (reply_token, msg) => {
 };
 
 const getGas = async () => {
-  request(
-    "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken",
-    { json: true },
-    (err, response, body) => {
-      console.log("statusCode", response.statusCode);
-      if (err) {
-        console.error("error", err);
-      } else {
-        console.log("body", body);
-        return body.result.safeGasPrice;
-      }
-    }
-  );
+  try {
+    const response = await axios.get(
+      "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken"
+    );
+    console.log(response);
+  } catch (err) {
+    console.log("error occurrded", err);
+  }
+
+  //   request(
+  //     "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken",
+  //     { json: true },
+  //     (err, response, body) => {
+  //       console.log("statusCode", response.statusCode);
+  //       if (err) {
+  //         console.error("error", err);
+  //       } else {
+  //         console.log("body", body);
+  //         return body.result.safeGasPrice;
+  //       }
+  //     }
+  //   );
 };
-app.post("/webhook", async (req, res) => {
+app.post("/webhook", (req, res) => {
   let reply_token = req.body.events[0].replyToken;
   console.log("getting gas fee...");
-  let gasFee = await getGas(reply_token);
+  let gasFee = getGas();
   console.log("this is gas Fee", gasFee);
   console.log("replying...");
   reply(reply_token, gasFee);
